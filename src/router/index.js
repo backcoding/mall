4c4aacb95e657717c4bfbee6a3162fe58027f287
backcoding <18310701744@163.com>
2020-01-02 17:15:09 +0800

    项目模块规划以及目录划分

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+const Home = () => import('../views/home/Home') 
+const Category = () => import('../views/category/Category') 
+const ShopCart = () => import('../views/cart/ShopCart') 
+const Porfile = () => import('../views/porfile/Porfile') 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/category',
+      component: Category
+    },
+    {
+      path: '/shopcart',
+      component: ShopCart
+    },
+    {
+      path: '/profile',
+      component: Porfile
+    },
 ]
 
 const router = new VueRouter({
