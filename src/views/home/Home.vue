b47807a8d9c8b0b3196cc6ede74fddd49d51c3fd
backcoding <18310701744@163.com>
2020-01-04 20:25:27 +0800

    RcommendView 首页推荐添加

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,35 +4,38 @@
       <div slot="center">淘宝阁</div>
     </nav-bar>
     <home-swiper :banners="barners" />
+    <recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
-import HomeSwiper from "./childComps/HomeSwiper";
-import { getHomeMultidata } from "network/home";
+import NavBar from "components/common/navbar/NavBar";//加载头部bar
+import HomeSwiper from "./childComps/HomeSwiper"; //加载轮播图
+import RecommendView from "./childComps/RecommendView" //加载推荐
+import { getHomeMultidata } from "network/home"; //加载aixo数据请求
 export default {
   name: "home",
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView
   },
   data() {
     return {
       barners: [],
       recommends: []
     };
-  }
+  },
 
-  // created() {
-  //   //1.请求多个数据
-  //   getHomeMultidata()
-  //     .then(res => {
-  //       //console.log(res)
-  //       this.barners = res.data.banner.list;
-  //       this.recommends = res.data.recommends.list;
-  //     })
-  // }
+  created() {
+    //1.请求多个数据
+    getHomeMultidata()
+      .then(res => {
+        console.log(res)
+        this.barners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+  }
 };
 </script>
 
