b5954363d81e3174b330834032d172b61d68548c
backcoding <18310701744@163.com>
2020-01-06 17:26:08 +0800

    提交 TabControl

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tab-control">
+    <div
+      v-for="(item, index) in titles"
+      :key="index"
+      class="tab-control-item"
+      :class="{acitve: index === cunrrentIndex}"
+      @click="tabclick(index)"
+    >
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      cunrrentIndex: 0
+    };
+  },
+  methods:{
+    tabclick(index){
+      this.cunrrentIndex = index;
+    }
+  }
+};
+</script>
+
+<style>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  height: 40px;
+  line-height: 40px;
+  background-color: #fff;
+}
+.tab-control-item {
+  flex: 1;
+}
+.tab-control-item span {
+  padding: 5px;
+}
+.acitve {
+  color: var(--color-high-text);
+}
+.acitve span {
+  border-bottom: 2px solid var(--color-tint);
+}
+</style>
